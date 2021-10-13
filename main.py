be2f0a9730bc268fb26f34fa48a8203e4a389d1d
HectorMCT <hectorchavezt@outlook.com>
2021-10-13 19:44:50 +0000

    Minim changes in main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pytest import main
 from arithmetic_arranger import arithmetic_arranger
 
 
-#print(arithmetic_arranger(['32 - 698', '1 - 3801', '45 + 43', '123 + 49', '988 + 40'], True))
+print(arithmetic_arranger(['32 - 698', '1 - 3801', '45 + 43', '123 + 49', '988 + 40'], True))
 
 
 # Run unit tests automatically
